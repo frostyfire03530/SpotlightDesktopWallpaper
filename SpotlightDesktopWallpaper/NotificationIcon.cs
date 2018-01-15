e05a2f7a74cb39c1792169d707669038f8af33d9
frostyfire03530 <frostyfire03530@gmail.com>
2018-01-15 15:51:25 -0600

    Added menu item to save wallpapers to local; Added two new methods and tweakds to code to adapt to new registry keys used after the Windows 10 Fall Creator Update.

--- SpotlightDesktopWallpaper/NotificationIcon.cs
+++ SpotlightDesktopWallpaper/NotificationIcon.cs
@@ -20,13 +20,27 @@ namespace SpotlightDesktopWallpaper{
 		private RegistryUtils.RegistryMonitor notificationMonitor;
 		#region Initialize icon and menu
 		public NotificationIcon(){
+			
 			notifyIcon = new NotifyIcon();
 			notificationMenu = new ContextMenu(InitializeMenu());
-			RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-			if(rk.GetValue("SpotlightDesktopWallpaper") != null){
+			RegistryKey autoStart = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+			if(autoStart.GetValue("SpotlightDesktopWallpaper") != null){
+			   	(notificationMenu.MenuItems[2]).Checked = true;
+			}
+			RegistryKey saveWallpaper = Registry.CurrentUser.OpenSubKey("SOFTWARE\\SpotlightDesktopWallpaper",true);
+			if(saveWallpaper == null){
+				Registry.CurrentUser.CreateSubKey("SOFTWARE\\SpotlightDesktopWallpaper");
+			}
+			else if(saveWallpaper.GetValue("SaveWallpaper") != null){
 			   	(notificationMenu.MenuItems[1]).Checked = true;
 			}
-			notificationMonitor = new RegistryUtils.RegistryMonitor(RegistryHive.CurrentUser, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative\\");
+			String registryPath = getRegistryPath();//"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative\\"
+			if (registryPath.Contains("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative\\")){
+				notificationMonitor = new RegistryUtils.RegistryMonitor(RegistryHive.CurrentUser, registryPath);
+			}
+			else{
+				notificationMonitor = new RegistryUtils.RegistryMonitor(RegistryHive.LocalMachine, registryPath);
+			}
 			notificationMonitor.RegChanged += OnRegChanged;
 			notificationMonitor.Start();
 			notifyIcon.DoubleClick += IconDoubleClick;
@@ -37,6 +51,7 @@ namespace SpotlightDesktopWallpaper{
 		private MenuItem[] InitializeMenu(){
 			MenuItem[] menu = new MenuItem[] {
 				new MenuItem("About", menuAboutClick),
+				new MenuItem("Save to Pictures", menuSaveClick),
 				new MenuItem("Start when Windows starts", menuStartClick),
 				new MenuItem("Exit", menuExitClick)
 			};
@@ -47,6 +62,95 @@ namespace SpotlightDesktopWallpaper{
 		/// <summary>Program entry point.</summary>
 		/// <param name="args">Command Line Arguments</param>
 		[STAThread]
+		public static string getLandscapePath(){
+			string landscapeAssetPath = "";
+			string landscapeAssetPathNew = "";
+			string landscapeAssetPathNewSub = "";
+			string subKeyNamePath = "";
+			try {
+				using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+				using (RegistryKey key = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative", true)){
+			       	if (key != null){
+						foreach(string subKey in key.GetSubKeyNames()){
+							RegistryKey subKeyNames = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative\\" + subKey + "\\", true);
+							if(subKeyNames.ValueCount > 4){
+								subKeyNamePath = subKey;
+								Object o = subKeyNames.GetValue("LandscapeAssetPath");
+								landscapeAssetPathNew = o.ToString();
+								break;
+							}
+						}
+					}
+				}
+			} catch (Exception ex) {
+				System.Diagnostics.Debug.Write(ex.ToString());
+			}
+			try {
+				using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+				using (RegistryKey key = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative\\" + subKeyNamePath + "\\", true)){
+					if (key != null){
+						String[] subKeyNamesArray = key.GetSubKeyNames();
+						int subKeyNamesArrayLastIndex = subKeyNamesArray.Length - 1;
+						RegistryKey subKeyNames = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative\\" + subKeyNamePath + "\\" + subKeyNamesArray[subKeyNamesArrayLastIndex] + "\\", true);
+						if(subKeyNames.ValueCount > 3){
+							Object o = subKeyNames.GetValue("landscapeImage");
+							landscapeAssetPathNewSub = o.ToString();
+							return landscapeAssetPathNewSub;
+						}
+						else{
+							return landscapeAssetPathNew;
+						}
+					}
+				}
+			} catch (Exception ex) {
+				System.Diagnostics.Debug.Write(ex.ToString());
+			}
+			try{
+			    using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative")){
+			        if (key != null){
+		        	    Object o = key.GetValue("LandscapeAssetPath");
+						landscapeAssetPath = o.ToString();
+						return landscapeAssetPath;
+			        }
+			    }
+			}
+			catch (Exception ex){
+				System.Diagnostics.Debug.Write(ex.ToString());
+			}
+			return "";
+		}
+		public static string getRegistryPath(){
+			string registryPath = "";
+			string registryPathNew = "";
+			try {
+				using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+				using (RegistryKey key = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative", true)){
+			       	if (key != null){
+						foreach(string subKey in key.GetSubKeyNames()){
+							RegistryKey subKeyNames = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative\\" + subKey + "\\", true);
+							if(subKeyNames.ValueCount > 4){
+								registryPathNew = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Creative\\" + subKey + "\\";
+								return registryPathNew;
+							}
+						}
+					}
+				}
+			} catch (Exception ex) {
+				System.Diagnostics.Debug.Write(ex.ToString());
+			}
+			try{
+			    using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative")){
+			        if (key != null){
+						registryPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative";
+						return registryPath;
+			        }
+			    }
+			}
+			catch (Exception ex){
+				System.Diagnostics.Debug.Write(ex.ToString());
+			}
+			return "";
+		}
 		public static void Main(string[] args){
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
@@ -54,9 +158,15 @@ namespace SpotlightDesktopWallpaper{
 			// Please use a unique name for the mutex to prevent conflicts with other programs
 			using (Mutex mtxSpotlightDesktopWallpaper = new Mutex(true, "SpotlightDesktopWallpaper", out isFirstInstance)) {
 				if(isFirstInstance){
+					MenuItem saveWallpaperItem = new MenuItem();
+					saveWallpaperItem.Checked = false;
 					NotificationIcon notificationIcon = new NotificationIcon();
 					notificationIcon.notifyIcon.Visible = true;
-					ApplicationContext wallpaperApplicationContext = new wallpaperApplicationContext();
+					RegistryKey saveWallpaper = Registry.CurrentUser.OpenSubKey("SOFTWARE\\SpotlightDesktopWallpaper",true);
+					if(saveWallpaper.GetValue("SaveWallpaper") != null){
+						saveWallpaperItem.Checked = true;
+					}
+					ApplicationContext wallpaperApplicationContext = new wallpaperApplicationContext(saveWallpaperItem);
 					Application.Run();
 					notificationIcon.notifyIcon.Dispose();
 				} 
@@ -69,16 +179,26 @@ namespace SpotlightDesktopWallpaper{
 		#region Event Handlers
 		private void menuAboutClick(object sender, EventArgs e){
 			const string aboutBoxCaption = " About SpotlightDesktopWallpaper";
-			const string aboutBoxMessage = "Copyright 2016 Nathan Waters (frostyfire03530)\n\nLicensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\nhttp://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.Copyright 2016\n\nHome Page: http://nathanjwaters.com\n\nOriginal sources: https://code.msdn.microsoft.com/windowsapps/CSSetDesktopWallpaper-2107409c/sourcecode?fileId=21700&pathId=734742078\nhttp://www.codeproject.com/Articles/4502/RegistryMonitor-a-NET-wrapper-class-for-RegNotifyC\nhttp://stackoverflow.com/questions/18232972/how-to-read-value-of-a-registry-key-c-sharp\nhttp://stackoverflow.com/questions/674628/how-do-i-set-a-program-to-launch-at-startup\n\nWritten in C# using SharpDevelop 5.1.0\nBuild: 1.0.5";
+			const string aboutBoxMessage = "Copyright 2016 Nathan Waters (frostyfire03530)\n\nLicensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\nhttp://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.Copyright 2016\n\nHome Page: http://nathanjwaters.com\n\nOriginal sources: https://code.msdn.microsoft.com/windowsapps/CSSetDesktopWallpaper-2107409c/sourcecode?fileId=21700&pathId=734742078\nhttp://www.codeproject.com/Articles/4502/RegistryMonitor-a-NET-wrapper-class-for-RegNotifyC\nhttp://stackoverflow.com/questions/18232972/how-to-read-value-of-a-registry-key-c-sharp\nhttp://stackoverflow.com/questions/674628/how-do-i-set-a-program-to-launch-at-startup\n\nWritten in C# using SharpDevelop 5.1.0\nBuild: 1.0.6";
 			MessageBox.Show(aboutBoxMessage,aboutBoxCaption);
 		}
-		private void menuStartClick(object sender, EventArgs e){
+		private void menuSaveClick(object sender, EventArgs e){
 			if((notificationMenu.MenuItems[1]).Checked == false){
 				(notificationMenu.MenuItems[1]).Checked = true;
-				SetStartup();
+				SetSave();
 			}
 			else{
 				(notificationMenu.MenuItems[1]).Checked = false;
+				SetSave();
+			}
+		}
+		private void menuStartClick(object sender, EventArgs e){
+			if((notificationMenu.MenuItems[2]).Checked == false){
+				(notificationMenu.MenuItems[2]).Checked = true;
+				SetStartup();
+			}
+			else{
+				(notificationMenu.MenuItems[2]).Checked = false;
 				SetStartup();
 			}
 		}
@@ -90,11 +210,20 @@ namespace SpotlightDesktopWallpaper{
 			MessageBox.Show("Windows Spotlight is being monitored for a change in the background to load into the desktop","SpotlightDesktopWallpaper");
 		}
 		private void OnRegChanged(object sender, EventArgs e){
-			ApplicationContext wallpaperApplicationContext = new wallpaperApplicationContext();
+			ApplicationContext wallpaperApplicationContext = new wallpaperApplicationContext((notificationMenu.MenuItems[1]));
 		}
+		private void SetSave(){
+	        RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\SpotlightDesktopWallpaper", true);
+	        if((notificationMenu.MenuItems[1]).Checked){
+	        	rk.SetValue("SaveWallpaper", "true");
+	        }
+	        else{
+	        	rk.DeleteValue("SaveWallpaper",false);
+	        }
+	    }
 		private void SetStartup(){
 	        RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-	        if((notificationMenu.MenuItems[1]).Checked){
+	        if((notificationMenu.MenuItems[2]).Checked){
 	        	rk.SetValue("SpotlightDesktopWallpaper", Application.ExecutablePath.ToString());
 	        }
 	        else{
@@ -103,19 +232,18 @@ namespace SpotlightDesktopWallpaper{
 	    }
 		#endregion
 	}
+
 	public class wallpaperApplicationContext : ApplicationContext{
-		public wallpaperApplicationContext(){
+		public wallpaperApplicationContext(MenuItem saveWallpaper){
 			try{
-			    using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative")){
-			        if (key != null){
-		        	    Object o = key.GetValue("LandscapeAssetPath");
-						string landscapeAssetPath;
-						landscapeAssetPath = o.ToString();
-		                if (o != null && landscapeAssetPath!=""){
-		                	Wallpaper.Set(landscapeAssetPath, Wallpaper.WallpaperStyle.Fill);
-		                }
-			        }
-			    }
+				//getLandscapePath newPath = new getLandscapePath();
+				String landscapeAssetPath = SpotlightDesktopWallpaper.NotificationIcon.getLandscapePath();//newPath.ToString();
+                if (landscapeAssetPath!=""){
+                	Wallpaper.Set(landscapeAssetPath, Wallpaper.WallpaperStyle.Fill);
+                	if(saveWallpaper.Checked == true){
+                	   	Wallpaper.Save(landscapeAssetPath);
+                	}
+                }
 			}
 			catch (Exception ex){
 				System.Diagnostics.Debug.Write(ex.ToString());
